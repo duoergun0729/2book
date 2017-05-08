81e8e362532b53924aa6952a42e38ae4bfd20547
duoergun0729 <250261953@qq.com>
2017-05-09 07:13:25 +0800

    svm

--- code/mnist.py
+++ code/mnist.py
@@ -11,11 +11,12 @@ import os
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+from sklearn import svm
 
 # Data loading and preprocessing
 import tflearn.datasets.mnist as mnist
 
-def cnn(X, Y, testX, testY ):
+def do_cnn_2d(X, Y, testX, testY ):
     # Building convolutional network
     network = input_data(shape=[None, 28, 28, 1], name='input')
     network = conv_2d(network, 32, 3, activation='relu', regularizer="L2")
@@ -36,27 +37,56 @@ def cnn(X, Y, testX, testY ):
     model = tflearn.DNN(network, tensorboard_verbose=0)
     model.fit({'input': X}, {'target': Y}, n_epoch=10,
                validation_set=({'input': testX}, {'target': testY}),
-               snapshot_step=100, show_metric=True, run_id='convnet_mnist')
+               snapshot_step=100, show_metric=True, run_id='mnist')
 
 def do_dnn_1d(x_train, y_train,x_test , y_test):
     print "DNN and 1d"
 
     # Building deep neural network
-    clf = MLPClassifier(solver='lbfgs',
-                        alpha=1e-5,
-                        hidden_layer_sizes = (15, 10),
-                        random_state = 1)
-    print  clf
+    input_layer = tflearn.input_data(shape=[None, 784])
+    dense1 = tflearn.fully_connected(input_layer, 64, activation='tanh',
+                                     regularizer='L2', weight_decay=0.001)
+    dropout1 = tflearn.dropout(dense1, 0.8)
+    dense2 = tflearn.fully_connected(dropout1, 64, activation='tanh',
+                                     regularizer='L2', weight_decay=0.001)
+    dropout2 = tflearn.dropout(dense2, 0.8)
+    softmax = tflearn.fully_connected(dropout2, 10, activation='softmax')
+
+    # Regression using SGD with learning rate decay and Top-3 accuracy
+    sgd = tflearn.SGD(learning_rate=0.1, lr_decay=0.96, decay_step=1000)
+    top_k = tflearn.metrics.Top_k(3)
+    net = tflearn.regression(softmax, optimizer=sgd, metric=top_k,
+                             loss='categorical_crossentropy')
+
+    # Training
+    model = tflearn.DNN(net, tensorboard_verbose=0)
+    model.fit(X, Y, n_epoch=10, validation_set=(testX, testY),
+              show_metric=True, run_id="mnist")
+
+def do_svm_1d(x_train, y_train,x_test, y_test):
+    print "SVM and 1d"
+    clf = svm.SVC(decision_function_shape='ovo')
+    print clf
     clf.fit(x_train, y_train)
     y_pred = clf.predict(x_test)
     print metrics.accuracy_score(y_test, y_pred)
-    print metrics.confusion_matrix(y_test, y_pred)
+    #print metrics.confusion_matrix(y_test, y_pred)
+
 
 if __name__ == "__main__":
     print "Hello MNIST"
-    #X, Y, testX, testY = mnist.load_data(one_hot=True)
     X, Y, testX, testY = mnist.load_data(one_hot=False)
-    #X = X.reshape([-1, 28, 28, 1])
-    #testX = testX.reshape([-1, 28, 28, 1])
+    #1d
+    print  testX
+    #do_dnn_1d(X, Y, testX, testY)
+    do_svm_1d(X, Y, testX, testY)
 
-    do_dnn_1d(X, Y, testX, testY)
+
+
+    #2d
+    X, Y, testX, testY = mnist.load_data(one_hot=True)
+    X = X.reshape([-1, 28, 28, 1])
+    testX = testX.reshape([-1, 28, 28, 1])
+
+    #cnn
+    #do_cnn_2d(X, Y, testX, testY)
