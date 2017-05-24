9b96cc8f40d19fc2a52e072c9b77a9fd67b137ab
maidou <250261953@qq.com>
2017-05-24 11:32:22 +0800

    add

--- code/webshell.py
+++ code/webshell.py
@@ -147,15 +147,11 @@ if __name__ == '__main__':
                         hidden_layer_sizes=(5, 2),
                         random_state=1)
 
-    print clf
+    #print clf
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=0)
     clf.fit(x_train, y_train)
     y_pred = clf.predict(x_test)
     do_metrics(y_test,y_pred)
-
-
-
-
 
 
     #do_check(x,y,clf)
