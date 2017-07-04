7daf2e768e0b4254c8659f8bf26f807ad855e4be
maidou <250261953@qq.com>
2017-07-04 15:10:16 +0800

    add

--- code/scanner-poc.py
+++ code/scanner-poc.py
@@ -96,10 +96,45 @@ def get_login_pages(keywords):
     #cats = ['sci.crypt']
     #newsgroups_train = fetch_20newsgroups(subset='train', categories=cats)
     newsgroups_train = fetch_20newsgroups(subset='train')
+    newsgroups_test = fetch_20newsgroups(subset='test')
+
+    newsgroups=[]
+    newsgroups.append(newsgroups_train.data)
+    newsgroups.append(newsgroups_test.data)
+    #newsgroups_train = fetch_20newsgroups()
+    #print len(newsgroups_train.data)
+    print newsgroups_train.data
     sentences=[re.findall("[a-z\-]+",s.lower()) for s in newsgroups_train.data]
+    #sentences = [s.lower().split() for s in newsgroups_train.data]
     #print sentences
 
-    model = gensim.models.Word2Vec(sentences, size=200, window=5, min_count=1, workers=4)
+    model = gensim.models.Word2Vec(sentences, size=200, window=5, min_count=1, workers=4,iter=20)
+
+    #print len(sentences)
+
+    for key in keywords:
+        print "[%s] most_similar:" % key
+        results=model.most_similar(positive=[key], topn=10)
+        for i in results:
+            print i
+
+def get_login_pages_imdb(keywords):
+
+    import gensim
+    import re
+    from tflearn.datasets import imdb
+
+    train, test, _ = imdb.load_data(path='imdb.pkl', n_words=10000,
+                                    valid_portion=0.1)
+
+    trainX, trainY = train
+    sentences=trainX
+    print len(sentences)
+    print sentences
+
+    model = gensim.models.Word2Vec(sentences, size=200, window=3, min_count=1, workers=4,iter=50)
+
+
 
     for key in keywords:
         print "[%s] most_similar:" % key
@@ -110,4 +145,5 @@ def get_login_pages(keywords):
 if __name__ == "__main__":
     print "Hello ai scanner poc"
     #generator_xss()
-    get_login_pages(["user","password","email"])
+    get_login_pages(["user","password","email","name"])
+    #get_login_pages_imdb(["user", "password", "email", "name"])
