9a0511d8e58e1960f9a5d65b26c87511a43d572c
maidou <250261953@qq.com>
2017-07-05 16:08:17 +0800

    add

--- code/dga.py
+++ code/dga.py
@@ -68,6 +68,38 @@ def get_feature_charseq():
 
     return x_train, x_test, y_train, y_test
 
+
+def get_aeiou(domain):
+    count = len(re.findall(r'[aeiou]', domain.lower()))
+    #count = (0.0 + count) / len(domain)
+    return count
+
+def get_uniq_char_num(domain):
+    count=len(set(domain))
+    #count=(0.0+count)/len(domain)
+    return count
+
+def get_uniq_num_num(domain):
+    count = len(re.findall(r'[1234567890]', domain.lower()))
+    #count = (0.0 + count) / len(domain)
+    return count
+
+def get_feature():
+    from sklearn import preprocessing
+    alexa=load_alexa()
+    dga=load_dga()
+    v=alexa+dga
+    y=[0]*len(alexa)+[1]*len(dga)
+    x=[]
+
+    for vv in v:
+        vvv=[get_aeiou(vv),get_uniq_char_num(vv),get_uniq_num_num(vv),len(vv)]
+        x.append(vvv)
+
+    x=preprocessing.scale(x)
+    x_train, x_test, y_train, y_test=train_test_split(x,y,test_size=0.4)
+    return x_train, x_test, y_train, y_test
+
 def get_feature_2gram():
     alexa=load_alexa()
     dga=load_dga()
@@ -77,6 +109,28 @@ def get_feature_2gram():
 
     CV = CountVectorizer(
                                     ngram_range=(2, 2),
+                                    token_pattern=r'\w',
+                                    decode_error='ignore',
+                                    strip_accents='ascii',
+                                    max_features=max_features,
+                                    stop_words='english',
+                                    max_df=1.0,
+                                    min_df=1)
+    x = CV.fit_transform(x)
+    x_train, x_test, y_train, y_test=train_test_split(x,y,test_size=0.4)
+
+    return x_train.toarray(), x_test.toarray(), y_train, y_test
+
+
+def get_feature_234gram():
+    alexa=load_alexa()
+    dga=load_dga()
+    x=alexa+dga
+    max_features=10000
+    y=[0]*len(alexa)+[1]*len(dga)
+
+    CV = CountVectorizer(
+                                    ngram_range=(2, 4),
                                     token_pattern=r'\w',
                                     decode_error='ignore',
                                     strip_accents='ascii',
@@ -127,18 +181,18 @@ def do_rnn(trainX, testX, trainY, testY):
     # Network building
     net = tflearn.input_data([None, max_document_length])
     net = tflearn.embedding(net, input_dim=10240000, output_dim=64)
-    net = tflearn.lstm(net, 64, dropout=0.8)
+    net = tflearn.lstm(net, 64, dropout=0.1)
     net = tflearn.fully_connected(net, 2, activation='softmax')
     net = tflearn.regression(net, optimizer='adam', learning_rate=0.001,
                              loss='categorical_crossentropy')
 
     # Training
-    model = tflearn.DNN(net, tensorboard_verbose=0)
+    model = tflearn.DNN(net, tensorboard_verbose=0,tensorboard_dir="dga_log")
     model.fit(trainX, trainY, validation_set=(testX, testY), show_metric=True,
-              batch_size=10,run_id="dga",n_epoch=5)
+              batch_size=10,run_id="dga",n_epoch=1)
 
     y_predict_list = model.predict(testX)
-    print y_predict_list
+    #print y_predict_list
 
     y_predict = []
     for i in y_predict_list:
@@ -153,11 +207,28 @@ def do_rnn(trainX, testX, trainY, testY):
 
 if __name__ == "__main__":
     print "Hello dga"
+    print "234-gram & mlp"
+    x_train, x_test, y_train, y_test = get_feature_234gram()
+    do_mlp(x_train, x_test, y_train, y_test)
+    """
+    print "text feature & nb"
+    x_train, x_test, y_train, y_test = get_feature()
+    do_nb(x_train, x_test, y_train, y_test)
+
+    print "text feature & xgboost"
+    x_train, x_test, y_train, y_test = get_feature()
+    do_xgboost(x_train, x_test, y_train, y_test)
+
+    print "text feature & mlp"
+    x_train, x_test, y_train, y_test = get_feature()
+    do_mlp(x_train, x_test, y_train, y_test)
+
+
     print "charseq & rnn"
     x_train, x_test, y_train, y_test = get_feature_charseq()
     do_rnn(x_train, x_test, y_train, y_test)
 
-"""
+
     print "2-gram & mlp"
     x_train, x_test, y_train, y_test = get_feature_2gram()
     do_mlp(x_train, x_test, y_train, y_test)
