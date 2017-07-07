42fbf3398707f3aea560d6f4f73bea835f68f6ad
duoergun0729 <250261953@qq.com>
2017-07-07 11:56:06 +0800

    add

---
+++ code/tools.py
@@ -0,0 +1,84 @@
+
+
+
+def use_keras():
+    import keras
+    from keras.datasets import mnist
+    from keras.models import Sequential
+    from keras.layers import Dense, Dropout
+    from keras.optimizers import RMSprop
+
+    batch_size = 128
+    num_classes = 10
+    epochs = 20
+
+    # the data, shuffled and split between train and test sets
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    x_train = x_train.reshape(60000, 784)
+    x_test = x_test.reshape(10000, 784)
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
+    print(x_train.shape[0], 'train samples')
+    print(x_test.shape[0], 'test samples')
+
+    # convert class vectors to binary class matrices
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
+
+    model = Sequential()
+    model.add(Dense(512, activation='relu', input_shape=(784,)))
+    model.add(Dropout(0.2))
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(10, activation='softmax'))
+
+    model.summary()
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=RMSprop(),
+                  metrics=['accuracy'])
+
+    history = model.fit(x_train, y_train,
+                        batch_size=batch_size,
+                        epochs=epochs,
+                        verbose=1,
+                        validation_data=(x_test, y_test))
+    score = model.evaluate(x_test, y_test, verbose=0)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
+
+def use_tflearn():
+    import tflearn
+
+    # Data loading and preprocessing
+    import tflearn.datasets.mnist as mnist
+    X, Y, testX, testY = mnist.load_data(one_hot=True)
+
+    # Building deep neural network
+    input_layer = tflearn.input_data(shape=[None, 784])
+    dense1 = tflearn.fully_connected(input_layer, 64, activation='tanh',
+                                     regularizer='L2', weight_decay=0.001)
+    dropout1 = tflearn.dropout(dense1, 0.8)
+    dense2 = tflearn.fully_connected(dropout1, 64, activation='tanh',
+                                     regularizer='L2', weight_decay=0.001)
+    dropout2 = tflearn.dropout(dense2, 0.8)
+    softmax = tflearn.fully_connected(dropout2, 10, activation='softmax')
+
+    # Regression using SGD with learning rate decay and Top-3 accuracy
+    sgd = tflearn.SGD(learning_rate=0.1, lr_decay=0.96, decay_step=1000)
+    top_k = tflearn.metrics.Top_k(3)
+    net = tflearn.regression(softmax, optimizer=sgd, metric=top_k,
+                             loss='categorical_crossentropy')
+
+    # Training
+    model = tflearn.DNN(net, tensorboard_verbose=0)
+    model.fit(X, Y, n_epoch=20, validation_set=(testX, testY),
+              show_metric=True, run_id="dense_model")
+
+if __name__ == "__main__":
+    print "Tools for deeplearning"
+    use_tflearn()
+    #use_keras()
